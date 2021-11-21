690bdaa63581f23e39d1db1a55203f4166eb6310
pvttrix <dipatricha@gmail.com>
2021-11-21 17:08:28 +0300

    create card scene

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Peek-a-Vue</h1>
+  <section class="game-board">
+    <Card
+      v-for="(card,index) in cardList"
+      :value="card"
+      :key="`card-${index}`"
+    />
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card,
+  },
+  setup () {
+   const cardList = []
+
+   for (let i = 0; i < 16; i++) {
+     cardList.push(i)
+   }
+
+   return {
+     cardList
+   }
   }
 }
 </script>
@@ -23,4 +40,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+
 </style>
